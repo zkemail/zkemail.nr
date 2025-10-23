3febd74e772a600ff6b0d0b2e213d0e4ed4c870e
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-10-23 12:57:19 +0100

    chore: add bb and noir version checking to scripts

--- scripts/info.sh
+++ scripts/info.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+NARGO_VERSION="1.0.0-beta.5"
+BB_VERSION="0.84.0"
+
+check_versions() {
+    if [ "$(nargo --version | grep "nargo version = $NARGO_VERSION")" != "nargo version = $NARGO_VERSION" ]; then
+        echo "Noir version is not $NARGO_VERSION, running noirup --version $NARGO_VERSION"
+        noirup --version $NARGO_VERSION
+    fi
+
+    if [ "$(bb --version)" != "v$BB_VERSION" ]; then
+        echo "BB version is not $BB_VERSION, running bbup --version $BB_VERSION"
+        bbup --version $BB_VERSION
+    fi
+}
+
 get_circuit_size() {
     project=$1
     
@@ -20,7 +35,11 @@ get_circuit_size() {
     popd > /dev/null
 }
 
+# Check the versions of Noir and BB
+check_versions
+
 cd $SCRIPT_DIR/../examples
+
 # Loop over every child folder in the examples directory
 for folder in *; do
     if [ -d "$folder" ]; then
